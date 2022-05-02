35257f0e26fba572492d8c4ecb0f5b857e6dd0bd
Azat-Farmanyan <azat.farmanyan2@mail.ru>
2022-05-02 19:00:29 +0400

    feat: fixed bugs for 1rs card modal vindow(lear more button)

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,37 +1,11 @@
-const open = document.getElementById('learn-more-open')
+const open_katrine = document.getElementById('learn-more-katrine')
 const modal_container = document.getElementById('modal-container')
-const close = document.getElementById('close')
+const close = document.getElementById('close-katrine')
 
-open.addEventListener('click', () => {
+open_katrine.addEventListener('click', () => {
    modal_container.classList.add('show')
 })
 
 close.addEventListener('click', () => {
    modal_container.classList.remove('show')
-})
-
-
-const open2 = document.getElementById('learn-more-open-jennifer')
-const modal_container2 = document.getElementById('modal-container-katrine')
-const close2 = document.getElementById('close-katrine')
-
-open2.addEventListener('click', () => {
-   modal_container2.classList.add('show')
-})
-
-close2.addEventListener('click', () => {
-   modal_container2.classList.remove('show')
-})
-
-
-const open3 = document.getElementById('learn-more-jennifer')
-const modal_container3 = document.getElementById('modal-container-katrine')
-const close3 = document.getElementById('close')
-
-open3.addEventListener('click', () => {
-   modal_container3.classList.add('show')
-})
-
-close3.addEventListener('click', () => {
-   modal_container3.classList.remove('show')
 })
