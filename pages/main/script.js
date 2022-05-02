60f657e691f051f6b28da9d215abc9ebcd9d91fd
Azat-Farmanyan <azat.farmanyan2@mail.ru>
2022-05-02 10:57:44 +0400

    feat: fixed bugs in burger(responsive)

--- pages/main/script.js
+++ pages/main/script.js
@@ -9,3 +9,29 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
    modal_container.classList.remove('show')
 })
+
+
+const open2 = document.getElementById('learn-more-open-jennifer')
+const modal_container2 = document.getElementById('modal-container-katrine')
+const close2 = document.getElementById('close-katrine')
+
+open2.addEventListener('click', () => {
+   modal_container2.classList.add('show')
+})
+
+close2.addEventListener('click', () => {
+   modal_container2.classList.remove('show')
+})
+
+
+const open3 = document.getElementById('learn-more-jennifer')
+const modal_container3 = document.getElementById('modal-container-katrine')
+const close3 = document.getElementById('close')
+
+open3.addEventListener('click', () => {
+   modal_container3.classList.add('show')
+})
+
+close3.addEventListener('click', () => {
+   modal_container3.classList.remove('show')
+})
