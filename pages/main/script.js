decdf402f5930bb38de4431bc04247a8688bb64c
Azat-Farmanyan <azat.farmanyan2@mail.ru>
2022-06-03 14:23:03 +0400

    feat: create modal block for Learn More Button

--- pages/main/script.js
+++ pages/main/script.js
@@ -11,7 +11,6 @@ async function getProducts() {
    // console.log(productsArray);
 
    renderProducts(productsArray)
-
 }
 let ourFrindLeftButton = document.querySelector('.left-button')
 let ourFrindRightButton = document.querySelector('.right-button')
@@ -38,7 +37,7 @@ function renderProducts(productsArray) {
          <div class="card">
            <img class="card-photo" src=" ${item.img}" alt="${item.name} pet">
            <div class="cart-name">${item.name}</div>
-           <button id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
+           <button cardID = "${item.id}" id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
          </div>`;
       lastCardID = item.id
       productsContainer.insertAdjacentHTML('beforeend', productHTML)
@@ -53,7 +52,7 @@ ourFrindLeftButton.addEventListener('click', e => {
    <div class="card">
       <img class="card-photo" src=" ${currentLastCard.img}" alt="${currentLastCard.name} pet">
       <div class="cart-name">${currentLastCard.name}</div>
-      <button id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
+      <button cardID = "${currentLastCard.id}" id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
    </div>
    `
    lastCardID = currentLastCard.id
@@ -63,13 +62,13 @@ ourFrindLeftButton.addEventListener('click', e => {
 })
 
 ourFrindRightButton.addEventListener('click', e => {
-   if (lastCardID === 7) lastCardID = 0
+   if (lastCardID === 8) lastCardID = 0
    const currentLastCard = productsArray[lastCardID]
    const currentAnimal = `
    <div class="card">
       <img class="card-photo" src=" ${currentLastCard.img}" alt="${currentLastCard.name} pet">
       <div class="cart-name">${currentLastCard.name}</div>
-      <button id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
+      <button cardID = "${currentLastCard.id}" id="learn-more-katrine" class="learn-more-katrine">Learn more</button>
    </div>
    `
    lastCardID = currentLastCard.id
@@ -80,3 +79,43 @@ ourFrindRightButton.addEventListener('click', e => {
 
 // Learn More 
 console.log(productsContainer);
+productsContainer.addEventListener('click', e => {
+
+   if (e.target.hasAttribute('cardID')) {
+      const currentClickedCardId = Number(e.target.getAttribute("cardID")) - 1
+      const currentAnimalInfo = productsArray[currentClickedCardId]
+      console.log(currentAnimalInfo);
+
+      const currentModalBlock = `
+      <div class="modal-container" id="modal-container">
+         <div class="modal">
+            <div class="modal-photo">
+               <img src="${currentAnimalInfo.img}" alt="${currentAnimalInfo.name} pet">
+            </div>
+            <div class="modal-text-box">
+               <div class="modal-txtbox-title">${currentAnimalInfo.name}</div>
+               <div class="modal-txtbox-subtitle">${currentAnimalInfo.type} - ${currentAnimalInfo.breed}</div>
+               <div class="modal-txtbox-main-text">
+                    ${currentAnimalInfo.description}
+               </div>
+               <div class="modal-txtbox-list">
+                  <ul class="modal-txtbox-list-item"> <span>- Age:</span> ${currentAnimalInfo.age}</ul>
+                  <ul class="modal-txtbox-list-item"> <span>- Inoculations:</span> ${currentAnimalInfo.inoculations}</ul>
+                  <ul class="modal-txtbox-list-item"> <span>- Diseases:</span> ${currentAnimalInfo.diseases}</ul>
+                  <ul class="modal-txtbox-list-item"> <span>- Parasites:</span> ${currentAnimalInfo.parasites}</ul>
+               </div>
+            </div>
+            <button closeModal id="close-katrine"><img closeModal src="../../assets/icons/close.png" alt="close"></button>
+         </div>
+      </div>`
+      productsContainer.insertAdjacentHTML('afterbegin', currentModalBlock)
+   }
+})
+const modalBlock = document.querySelector('.modal')
+
+productsContainer.addEventListener('click', e => {
+   console.log(e.target);
+   if (e.target.hasAttribute('closeModal')) {
+      productsContainer.querySelector('.modal-container').remove()
+   }
+})
